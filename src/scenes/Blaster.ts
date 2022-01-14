32530394d1b92240a76407e281fc771b3414a485
Nick Mandylas <me@nickmandylas.com>
2022-01-14 23:45:16 +1100

    added bullets + blasting

--- src/scenes/Blaster.ts
+++ src/scenes/Blaster.ts
@@ -3,6 +3,8 @@ import * as THREE from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 
+import Bullet from "../objects/Bullet";
+
 export default class BlasterScene extends THREE.Scene {
   private readonly mtlLoader = new MTLLoader();
   private readonly objLoader = new OBJLoader();
@@ -12,6 +14,11 @@ export default class BlasterScene extends THREE.Scene {
   private readonly keyDown = new Set<string>();
 
   private blaster?: THREE.Group;
+  private bulletMtl?: MTLLoader.MaterialCreator;
+
+  private bullets: Bullet[] = [];
+  private targets: THREE.Group[] = [];
+
   private directionVector = new THREE.Vector3();
 
   constructor(camera: THREE.PerspectiveCamera) {
@@ -23,6 +30,9 @@ export default class BlasterScene extends THREE.Scene {
   async initalize() {
     const targetMtl = await this.mtlLoader.loadAsync("assets/targetA.mtl");
     targetMtl.preload();
+
+    this.bulletMtl = await this.mtlLoader.loadAsync("assets/foamBulletB.mtl");
+    this.bulletMtl.preload();
 
     const t1 = await this.createTarget(targetMtl);
     t1.position.x = -1;
@@ -41,6 +51,7 @@ export default class BlasterScene extends THREE.Scene {
     t4.position.z = -3;
 
     this.add(t1, t2, t3, t4);
+    this.targets.push(t1, t2, t3, t4);
 
     this.blaster = await this.createBlaster();
     this.add(this.blaster);
@@ -65,6 +76,10 @@ export default class BlasterScene extends THREE.Scene {
 
   private handleKeyUp = (event: KeyboardEvent) => {
     this.keyDown.delete(event.key.toLocaleLowerCase());
+
+    if (event.key === " ") {
+      this.createBullet();
+    }
   };
 
   private updateInput() {
@@ -130,7 +145,75 @@ export default class BlasterScene extends THREE.Scene {
     return modelRoot;
   }
 
+  private async createBullet() {
+    if (!this.blaster) {
+      return;
+    }
+
+    if (this.bulletMtl) {
+      this.objLoader.setMaterials(this.bulletMtl);
+    }
+
+    const bulletModel = await this.objLoader.loadAsync(
+      "assets/foamBulletB.obj"
+    );
+
+    this.camera.getWorldDirection(this.directionVector);
+
+    const aabb = new THREE.Box3().setFromObject(this.blaster);
+    const size = aabb.getSize(new THREE.Vector3());
+
+    const vec = this.blaster.position.clone();
+    vec.y += 0.06;
+
+    bulletModel.position.add(
+      vec.add(this.directionVector.clone().multiplyScalar(size.z * 0.5))
+    );
+
+    bulletModel.children.forEach((child) => child.rotateX(Math.PI * -0.5));
+    bulletModel.rotation.copy(this.blaster.rotation);
+
+    this.add(bulletModel);
+
+    const b = new Bullet(bulletModel);
+    b.setVelocity(
+      this.directionVector.x * 0.2,
+      this.directionVector.y * 0.2,
+      this.directionVector.z * 0.2
+    );
+    this.bullets.push(b);
+  }
+
+  private updateBullets() {
+    for (let i = 0; i < this.bullets.length; ++i) {
+      const b = this.bullets[i];
+      b.update();
+
+      if (b.shouldRemove) {
+        this.remove(b.group);
+        this.bullets.splice(i, 1);
+        i--;
+      } else {
+        for (let j = 0; j < this.targets.length; ++j) {
+          const target = this.targets[j];
+
+          if (target.position.distanceToSquared(b.group.position) < 0.05) {
+            this.remove(b.group);
+            this.bullets.splice(i, 1);
+            i--;
+
+            target.visible = false;
+            setTimeout(() => {
+              target.visible = true;
+            }, 1000);
+          }
+        }
+      }
+    }
+  }
+
   update() {
     this.updateInput();
+    this.updateBullets();
   }
 }
